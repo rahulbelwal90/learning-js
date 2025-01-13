b7dad2b12d0015641c3186b3e90f174a266202cc
Rahul Belwal <rahulbelwal90@gmail.com>
2025-01-13 04:09:05 +0000

    datatype

---
+++ 01_Basics/02_dataType.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version;
+
+// alert(3+3)  // we are using nodejs, not browser
+
+console.log("Rahul")
+
+// Code readability should be high;
+
+let name = "Knox"
+let age = 21
+let isLoggedIn = false
+
+// Datatype
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value ... 
+// undefined
+// symbol => unique
+
+console.log(typeof "Rahul")
+console.log(typeof age)
+console.log(typeof undefined)   // undefine type is undefined
+console.log(typeof null)        // null is a type of OBJECT
