e26ff025ecd360b63a54625304d0cd886834e0b0
Rahul Belwal <rahulbelwal90@gmail.com>
2025-01-16 07:06:11 +0000

    learning comparison operator

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,28 @@
+// Comparison Operator
+console.log(2>1)   //greater than
+console.log(2<1)   //less than
+console.log(2>=1)  //greater equal to
+console.log(2<=1)  //less than equal to
+console.log(2==2)  //equal to
+console.log(2!=1)  //not equal to
+
+// === triple equal also compare their data type with their values
+console.log("2" === 1);
+console.log("5" === "5");
+console.log(2 === 2);
+
+
+// never use this type of comparison below;;;
+
+console.log("2" > 1);  // output: true , js is converting string into number;
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 0);
+
+
+
