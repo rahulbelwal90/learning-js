3e89f6b6009d4302ed725b7e10db2d3b54c80067
Rahul Belwal <rahulbelwal90@gmail.com>
2025-01-21 22:04:42 +0000

    Object Part 2

---
+++ 02_Basics/04_objects.js
@@ -0,0 +1,61 @@
+// singleton object , or with the help of constructor
+
+// const tinderUser = new Object()  // Singleton object
+const tinderUser = {}    //Non singleton object
+
+tinderUser.id = "abc123"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname: "Rahul",
+            lastname: "Belwal"
+        }
+    }
+}
+console.log(regularUser.fullname);  // { userfullname: { firstname: 'Rahul', lastname: 'Belwal' } }
+console.log(regularUser.fullname.userfullname.firstname);    // Rahul
+
+//combining objects
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"c", 4:"d"}
+
+// const obj3 = {obj1, obj2}                       //1st method
+
+// const obj3 = Object.assign({}, obj1,obj2)      // 2nd method
+
+const obj3 = {...obj1, ...obj2}                   // using spread operator
+console.log(obj3);
+
+
+// using object inside array
+
+const users = [
+    {
+        name: "rahul",
+        age: 18
+    },
+    {
+        name: "rakesh",
+        age: 18
+    },
+    {
+        name: "rohan",
+        age: 18
+    }
+]
+console.log(users[1].name);
+console.log(users[2].name);
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));       //[ 'id', 'name', 'isLoggedIn' ]
+console.log(Object.values(tinderUser));     //[ 'abc123', 'Sammy', false ]
+console.log(Object.entries(tinderUser));    //[ [ 'id', 'abc123' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'))   //true       
