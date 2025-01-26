df3201c5b3a1efe48627be1a7d4042ecdd71fe0c
Rahul Belwal <rahulbelwal90@gmail.com>
2025-01-26 13:29:52 +0000

    scope level and mini hoisting

--- 03_Basics/02_function.js
+++ 03_Basics/02_function.js
@@ -6,7 +6,7 @@ let a = 200                   // Global Scope
 if(true){
     let a = 10                // Local Scope   : here both variable `a` having different value
     const b = 20
-    var c = 30
+    var c = 30     // var is not used inside a scope because it leak the local data outside the scope....
     console.log("Local a: ", a);
 }
 
@@ -15,3 +15,43 @@ if(true){
 console.log(c);      //output: 30                      // drawback of using var keyword .....
 
 console.log("Global a: ",a);
+
+//......................Scope level and Mini Hoisting......................
+
+function one(){
+    const username = "Rahul"
+    
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+    }
+    two()
+    // console.log(website);          // gives an error because we are using website outside its scope
+}
+one()
+
+if(true){
+    const username = "Rahul"
+    if(username === "Rahul"){
+        const website = "Youtube"
+        console.log(username + website );      // output: RahulYoutube
+    }
+    // console.log(website);      // gives error , using outside its scope
+}
+// console.log(username);     // gives error , using outside its scope
+
+
+//................................INTRESTING........................................
+
+
+console.log(addOne(5));           //we can call simple function before its definition
+
+function addOne(num){             // simple traditional function
+    return num+1
+}
+
+// addTwo()                     //cant call this type of function before its definition , gives an error
+const addTwo = function(num){      //declaring function in a variable
+    return num + 2
+}
+console.log(addTwo(5));         // this way of call funtion will work
