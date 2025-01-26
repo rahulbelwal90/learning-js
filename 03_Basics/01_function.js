0acfe64552df7c8e4b121ca541253572031fc914
Rahul Belwal <rahulbelwal90@gmail.com>
2025-01-26 10:22:12 +0000

    object and array handling using function

--- 03_Basics/01_function.js
+++ 03_Basics/01_function.js
@@ -1,3 +1,4 @@
+
 function sayMyName(){
     console.log("R");
     console.log("A");
@@ -37,3 +38,39 @@ function loginUserMessage(username = "sam"){
 // console.log(loginUserMessage("Rahul"));
 console.log(loginUserMessage());      //output:sam just logged in
 console.log(loginUserMessage("Rahul"));  //output:Rahul just logged in :new value is override on previous
+
+
+//...............................Function with Object and Array .......................................
+
+function calculateCartPrice(...num1) {                            // using rest operator ...
+    return num1
+}
+console.log(calculateCartPrice(100,200,300));
+
+
+// Handling Object using function
+
+const user = {                 // creating object
+    username: "Rahul",
+    price: 999
+}
+
+function handleObject(anyObject){
+    console.log(`Username is : ${anyObject.username} , Price is : ${anyObject.price}`);
+}
+
+handleObject(user);               // 1st way to passing object
+handleObject({                    // 2nd way to passing object
+    username: "Sam",
+    price: 199
+})
+
+// Handling Array using Function
+
+const myNewArray = [100,200,300,400]
+function returnedSecValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnedSecValue(myNewArray));                  // passing array using funtion
+console.log(returnedSecValue([10,20,30,40]));
